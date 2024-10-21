7588cff9514cc24d4579e144e28c6a2e906417eb
altarulas <altarulas66@gmail.com>
2024-10-21 20:25:43 +0300

    fix: fix build

--- components/pages/Home.tsx
+++ components/pages/Home.tsx
@@ -1,10 +1,4 @@
-import { authOptions } from "@/lib/auth";
-import { getServerSession } from "next-auth";
-
 export const HomePage = async () => {
-  const session = await getServerSession(authOptions);
-  console.log("session on home: ", session);
-
   return (
     <div className="w-full h-screen overflow-auto flex flex-col justify-center items-center">
       <div>Welcome to Home Page</div>
--- components/pages/Login.tsx
+++ components/pages/Login.tsx
@@ -1,11 +1,6 @@
 import { Login } from "../login";
-import { authOptions } from "@/lib/auth";
-import { getServerSession } from "next-auth";
 
 export const LoginPage = async () => {
-  const session = await getServerSession(authOptions);
-  console.log("session on login: ", session);
-
   return (
     <div className="w-full h-screen overflow-auto flex justify-center items-center">
       <Login />
